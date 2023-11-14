8391eeed85089baf3c5651cc4ab7187d37e96048
Tamils293 <75575608+Tamils293@users.noreply.github.com>
2023-11-14 18:07:32 +0530

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,71 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import next from "./tech/nextjs.png"
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import hackerNews from "./hackernews.png"
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import Perpetuuiti from "./company/perpetuuiti_logo.jfif";
+import MMP from "./company/magicmenporulprivatelimited_logo.jfif"
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import HiBro from "./hibro.png"
+import tindog from "./tindog.png"
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  next,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  Perpetuuiti,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  MMP,
+  hackerNews,
+  HiBro,
+  tripguide,
+  tindog
+};
---
+++ src/constants/index.js
@@ -0,0 +1,276 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  next,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  hackerNews,
+  jobit,
+  tripguide,
+  threejs,
+  Perpetuuiti,
+  MMP,
+  HiBro,
+
+  tindog,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Wordpress Developer",
+    icon: backend,
+  },
+  {
+    title: "Technical Reviewer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "nextjs",
+    icon: next,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full stack Development Intern",
+    company_name: "Magic Menporul Software Solutions Private Limited",
+    icon: MMP,
+    iconBg: "#383E56",
+    date: "Jun 2020 - Feb 2021",
+    points: [
+
+
+
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Created two projects: \"Hibro Home Services\" and \"Propdealz,\" a real estate registration app, both developed using React.",
+
+    ],
+  },
+  {
+    title: "Frontend Developer",
+    company_name: "Magic Menporul Software Solutions Private Limited",
+    icon: MMP,
+    iconBg: "#E6DEDD",
+    date: "Feb 2021 - Jun 2021",
+    points: [
+      "Continued development and maintenance of React-based web applications.",
+      "Engaged in collaborative efforts with diverse teams for product enhancements.",
+      "Contributed to the expansion of previously developed projects, ensuring quality and functionality.",
+
+    ],
+  },
+  {
+    title: "UI Developer",
+    company_name: "Perpetuuiti Technosoft PTE",
+    icon: Perpetuuiti,
+    iconBg: "#383E56",
+    date: "Jun 2021 - Jun 2023",
+    points: [
+      "Instrumental in crafting user interfaces for \"Continuity Patrol,\" a DRM-focused application.",
+      "Ensured seamless UI experiences, focusing on responsive design and cross-browser compatibility",
+      "Extended React.js expertise to elevate the platform's visual and functional aspects.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Senior UI Developer",
+    company_name: "Perpetuuiti Technosoft PTE",
+    icon: Perpetuuiti,
+    iconBg: "#E6DEDD",
+    date: "Jun 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Hacker News",
+    description:
+      " \"Hacker News\" is a site for tech and business news by Y Combinator. The Hacker News Replica, built with React and Tailwind CSS, offers tech fans a familiar place to discuss the latest in a cool way.It's like a twin of the famous Hacker News by Y Combinator ",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: hackerNews,
+    source_code_link: "https://github.com/Tamils293/Hacker-News",
+  },
+  {
+    name: "Hibro HomeServices",
+    description:
+      "\"Hibro HomeServices,\" built with React, is a comprehensive platform providing various household services. Users enjoy an easy-to-use interface for scheduling and managing home tasks. HiBro offers quality home services in Chennai.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "javascript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: HiBro,
+    source_code_link: "https://hibroservices.com/",
+  },
+  {
+    name: "TinDog",
+    description:
+      "\"Tindog\" is a playful project - think Tinder for dogs. Created with HTML, CSS, and Bootstrap for practice, it's a fun way to connect dogs and their owners.",
+    tags: [
+      {
+        name: "html5",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css3",
+        color: "green-text-gradient",
+      },
+      {
+        name: "bootstrap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tindog,
+    source_code_link: "https://github.com/Tamils293/tindog",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
